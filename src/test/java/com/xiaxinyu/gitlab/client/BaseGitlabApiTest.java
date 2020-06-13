922d74f7880a8c9d1f342c2cfc5908efa44eebbc
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2020-06-13 19:47:31 +0800

    add test module

---
+++ src/test/java/com/xiaxinyu/gitlab/client/BaseGitlabApiTest.java
@@ -0,0 +1,9 @@
+package com.xiaxinyu.gitlab.client;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {GitlabClientApplication.class})
+public class BaseGitlabApiTest {
+}
